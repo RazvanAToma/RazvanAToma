36bdd87e66d238363243ea2e69d5239b94e9b065
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2023-08-11 12:54:35 +0200

    hcv

---
+++ Python/spyder/Variabler og datatyper.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 19:42:51 2023
+
+@author: Razvan
+"""
+
+# Function - Funksjon
+
+import numpy as np
+
+x = 2.0
+
+y = np.sqrt(x)
+
+print(y)
+
+
+
+
+# Method - Metode
+
+L = [0, 1]
+L.append(5)
+
+L
