d0287570d09bd694ff6c527cc646a5f4a6e4a462
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2023-09-06 15:50:39 +0200

    h

--- Python/spyder/Variabler og datatyper.py
+++ Python/spyder/Variabler og datatyper.py
@@ -16,9 +16,36 @@ y = np.sqrt(x)
 print(y)
 
 
-
-
 # Method - Metode
 
 L = [0, 1]
 L.append(5)
+
+
+
+# Tuples - Tupler
+
+T = (1.0, "a")
+
+T1 = (1.0, "a")
+T2 = (2.0, "b")
+
+T = T1 + T2
+
+print(T)
+
+
+(tall1, tekst1) = (1.0, "a")
+
+print(tall1) ; print(tekst1)
+
+
+
+# Dictionary
+
+D = {}
+
+D["Lag"] = "Odd"
+D["Poeng"] = 100
+
+print(D)
