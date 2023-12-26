2ffa10d342ef4b1e9fe1ead82dab0c28ea4db9c7
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2023-12-26 20:48:09 +0100

    pyhthon

--- python/IT 2/1A Tall og tekster/Oppgaver/Oppgave14.py
+++ python/IT 2/1A Tall og tekster/Oppgaver/Oppgave14.py
@@ -1,1 +1,7 @@
 
+k1 = float(input("Katet 1:"))
+k2 = float(input("Katet 2:"))
+
+h = (k1**2 + k2**2)**(1/2)
+
+print(h)
---
+++ python/IT 2/1A Tall og tekster/Oppgaver/Oppgave15.py
@@ -0,0 +1,13 @@
+""" import math as m
+
+KATET1 = 7
+
+ALPHADEG = 60
+
+ALPHARAD = m.radians(ALPHADEG)
+
+x = KATET1 / ALPHARAD
+
+print(m.cos(x)) """
+
+# ???????????
